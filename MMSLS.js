e664b4de993cd31b6951bb39c15efcdfa1f60ccd
Linzi666 <109735520+Linzi666@users.noreply.github.com>
2022-10-28 15:29:34 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,9 +1,9 @@
 
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=21270100&pid=0fd5b27ed44ac8dab29f43bd36b8d30a&tid=d0440d8d7cb51108f909864ce1dd2939",
-    "https://www.maimemo.com/share/page?uid=21270100&pid=0fd5b27ed44ac8dab29f43bd36b8d30a&tid=d0440d8d7cb51108f909864ce1dd2939",
-    "https://www.maimemo.com/share/page?uid=21270100&pid=0fd5b27ed44ac8dab29f43bd36b8d30a&tid=d0440d8d7cb51108f909864ce1dd2939",
+    "https://www.maimemo.com/share/page?uid=21270100&pid=afa31488bb3cbb430d648be0fc55800f&tid=9479598cf46d33ca7179f3010a62f225",
+    "https://www.maimemo.com/share/page?uid=21270100&pid=afa31488bb3cbb430d648be0fc55800f&tid=9479598cf46d33ca7179f3010a62f225",
+    "https://www.maimemo.com/share/page?uid=21270100&pid=afa31488bb3cbb430d648be0fc55800f&tid=9479598cf46d33ca7179f3010a62f225",
   ]
   /**
    * 生成随机数字
